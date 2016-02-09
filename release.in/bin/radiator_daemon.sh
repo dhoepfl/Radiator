ecc258d3690e3721be30670e21d97922c7a934e5
Daniel Höpfl <daniel@hoepfl.de>
2016-02-09 17:15:21 +0100

    Install path expansion in scripts

--- release.in/bin/radiator_daemon.sh
+++
@@ -1,9 +0,0 @@
-#!  /bin/bash
-
-PATH="$PATH:/home/radiator/opt/bin"
-
-radiator \
-	-o >(analyseStream.sh | mysql >/home/radiator/opt/var/log/radiator.log 2>&1) \
-	/home/radiator/source/radiator/log.log.dd &
-
-echo $! >/home/radiator/opt/var/run/radiator.pid
